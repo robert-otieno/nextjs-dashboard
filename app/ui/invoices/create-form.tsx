2a66f457a421274a40591cf3aa06240033ed4d28
Robert Otieno <robertotieno.or@gmail.com>
2023-12-10 09:46:50 +0300

    adding authentication

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -140,11 +140,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         </fieldset>
 
         {/* Error handling */}
-        {/* <div id="amount-error" aria-live="polite" aria-atomic="true"> */}
-        {state.message && (
-          <p className="mt-2 text-sm text-red-500">{state.message}</p>
-        )}
-        {/* </div> */}
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          )}
+        </div>
       </div>
 
       <div className="mt-6 flex justify-end gap-4">
